5ab5da1094b4654c2e5f44b13ff146086afe887d
Osha Groetz <ogroetz@gmail.com>
2023-05-05 10:20:40 -0500

    styling

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -14,9 +14,49 @@ import '../box-styles.css'
 // 🐨 also use the style prop to make the font italic
 // 💰 Here are available style attributes: backgroundColor, fontStyle
 
-const smallBox = <div className="box box--small" style={{backgroundColor: 'lightblue', fontStyle: 'italic'}}>small lightblue box</div>
-const mediumBox = <div className="box box--medium" style={{backgroundColor: 'pink', fontStyle: 'italic'}}>medium pink box</div>
-const largeBox = <div className="box box--large" style={{backgroundColor: 'orange', fontStyle: 'italic'}}>large orange box</div>
+// const smallBox = <div className="box box--small" style={{backgroundColor: 'lightblue', fontStyle: 'italic'}}>small lightblue box</div>
+// const mediumBox = <div className="box box--medium" style={{backgroundColor: 'pink', fontStyle: 'italic'}}>medium pink box</div>
+// const largeBox = <div className="box box--large" style={{backgroundColor: 'orange', fontStyle: 'italic'}}>large orange box</div>
+
+// function App() {
+//   return (
+//     <div>
+//       {smallBox}
+//       {mediumBox}
+//       {largeBox}
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+// function Box(props) {
+//   return (
+//     // <div className="box" {...props} /> // spreading the props like this overrides all the properties before it in case of conflict
+//   )
+// }
+// ---------------------------------------------------------------------------
+// function Box({className = '', style, ...otherProps}) { // set className to default so that it never comes through as undefined
+//   return (
+//         <div className={`box ${className}`.trim()} style={{fontStyle: 'italic', ...style}} {...otherProps} /> // className value is a temporal literate 
+//   )
+// }
+
+// const smallBox = <Box className="box--small" style={{backgroundColor: 'lightblue'}}>small lightblue box</Box>
+// const mediumBox = <Box className="box--medium" style={{backgroundColor: 'pink'}}>medium pink box</Box>
+// const largeBox = <Box className="box--large" style={{backgroundColor: 'orange'}}>large orange box</Box>
+// ---------------------------------------------------------------------------
+function Box({size, className = '', style, ...otherProps}) {
+  const sizeClassName = size ? `box--${size}` : ''
+  return (
+        <div className={`box ${className} ${sizeClassName}`.trim()} style={{fontStyle: 'italic', ...style}} {...otherProps} />
+  )
+}
+
+const smallBox = <Box size="small" style={{backgroundColor: 'lightblue'}}>small lightblue box</Box>
+const mediumBox = <Box size="medium" style={{backgroundColor: 'pink'}}>medium pink box</Box>
+const largeBox = <Box size="large" style={{backgroundColor: 'orange'}}>large orange box</Box>
 
 function App() {
   return (
@@ -24,8 +64,18 @@ function App() {
       {smallBox}
       {mediumBox}
       {largeBox}
+      <Box>Unsized Box</Box>
     </div>
   )
 }
 
 export default App
+
+// in html you pass a string to the inline 'style' prop
+  // class attribute
+// in React you pass an object of CSS
+  // in jsx you use the className attribute
+
+  // in the console if you have an element selected, there will be a '$0' in light grey after the highlighted element (Elements tab)
+  // if you type $0 in the console, it references the element you have selected
+  // type:  $0.className or $0.style
